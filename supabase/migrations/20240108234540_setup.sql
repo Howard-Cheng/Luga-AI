8a7d9e58d54e6f56bb393708d414044ed7774d2c
Howard <cheng.howard1@gmail.com>
2024-06-23 13:14:49 -0500

    Commit

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -46,18 +46,18 @@ USING (auth.uid()::text = owner_id::text);
 -- Function to delete a storage object
 CREATE OR REPLACE FUNCTION delete_storage_object(bucket TEXT, object TEXT, OUT status INT, OUT content TEXT)
 RETURNS RECORD
-LANGUAGE 'plpgsql'
+LANGUAGE plpgsql
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'http://supabase_kong_chatbotui:8000';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU'
+  project_url TEXT := 'https://msdomrdbyvlrddjsjshp.supabase.co';
+  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zZG9tcmRieXZscmRkanNqc2hwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxOTEwMTkzNCwiZXhwIjoyMDM0Njc3OTM0fQ.QkyBaB9MiCbSe7BHc5YbRWju-qWfw71ul02dJSllITw';
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
-      INTO status, content
-           result.status::INT, result.content::TEXT
-      FROM extensions.http((
+      result.status::INT, result.content::TEXT
+  INTO status, content
+  FROM extensions.http((
     'DELETE',
     url,
     ARRAY[extensions.http_header('authorization','Bearer ' || service_role_key)],
@@ -65,6 +65,7 @@ BEGIN
     NULL)::extensions.http_request) AS result;
 END;
 $$;
+
 
 -- Function to delete a storage object from a bucket
 CREATE OR REPLACE FUNCTION delete_storage_object_from_bucket(bucket_name TEXT, object_path TEXT, OUT status INT, OUT content TEXT)
